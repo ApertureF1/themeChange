1cb931c27426f777a12cbc72a90a072781b952e7
ApertureF1 <lijia.ice@gmail.com>
2020-08-09 11:10:17 +0800

    create theme project

---
+++ app/src/main/java/com/example/injordemo/SkinApplication.java
@@ -0,0 +1,14 @@
+package com.example.injordemo;
+
+import android.app.Application;
+
+import com.injor.main.SkinManager;
+
+
+public class SkinApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		SkinManager.getInstance().init(this).addProcessor(new BrightnessProcessor());
+	}
+}
